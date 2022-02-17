4955efbe4c42e290ab42586cf04a2a0a36f3caf8
Kumar100966 <adrian.etwaroo@gmail.com>
2022-02-17 09:52:04 -0400

    Working on animations using Gsap and ScrollMagic

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,31 @@ const mainHeaderChildren = document.querySelector(".main").children;
 const logo = mainHeaderChildren[0];
 const navButton = mainHeaderChildren[1];
 const navOverlay = mainHeaderChildren[2];
+const slides = document.querySelectorAll(".body");
 
-navButton.addEventListener("click", (e) => {
-  console.log(e.target);
-  e.target.classList.toggle("active");
-  logo.classList.toggle("active");
-  navOverlay.classList.toggle("active");
+let sceneController = new ScrollMagic.Controller();
+
+navButton.addEventListener("click", function () {
+  const activeClass = "active";
+  this.classList.toggle(activeClass);
+  logo.classList.toggle(activeClass);
+  navOverlay.classList.toggle(activeClass);
+});
+
+slides.forEach((slide, i) => {
+  const covers = slide.querySelectorAll(".cover");
+  const timeline = gsap.timeline();
+  timeline.fromTo(covers, { x: "0%" }, { duration: 1, x: "100%" });
+  const scene = new ScrollMagic.Scene({
+    triggerElement: slide,
+    triggerHook: "onCenter",
+  });
+  scene.setTween(timeline);
+  scene.addIndicators({
+    colorStart: "white",
+    colorEnd: "white",
+    colorTrigger: "white",
+    name: "slide",
+  });
+  sceneController.addScene(scene);
 });
