bd25de50ed22fc355639ebdc745c59494fbd1f8f
Kumar100966 <adrian.etwaroo@gmail.com>
2022-02-17 13:57:39 -0400

    Implemented Slide Animation

--- scripts/app.js
+++ scripts/app.js
@@ -18,6 +18,23 @@ const timeline = gsap.timeline({ duration: 1 });
 timeline.fromTo(mainHeader, { y: "-100%" }, { y: "0%" });
 
 for (let i = 0; i < slides.length; i++) {
+  const pinScene = new ScrollMagic.Scene({
+    triggerElement: slides[i],
+    triggerHook: "onLeave",
+    duration: "200%",
+  });
+  pinScene.setPin(slides[i], {
+    pushFollowers: true,
+  });
+  // pinScene.addIndicators({
+  //   colorStart: "white",
+  //   colorEnd: "white",
+  //   colorTrigger: "white",
+  //   name: "pin",
+  //   indent: 400,
+  // });
+  sceneController.addScene(pinScene);
+
   const covers = slides[i].querySelectorAll(".cover");
   const removeCoversTimeline = gsap.timeline({
     duration: 1,
@@ -26,18 +43,14 @@ for (let i = 0; i < slides.length; i++) {
   const coversScene = new ScrollMagic.Scene({
     triggerElement: slides[i],
     triggerHook: "onCenter",
-    // duration: "100%",
   });
   coversScene.setTween(removeCoversTimeline);
-  // coversScene.setPin(slides[i], {
-  //   pushFollowers: true,
+  // coversScene.addIndicators({
+  //   colorStart: "white",
+  //   colorEnd: "white",
+  //   colorTrigger: "white",
+  //   name: "slide",
   // });
-  coversScene.addIndicators({
-    colorStart: "white",
-    colorEnd: "white",
-    colorTrigger: "white",
-    name: "slide",
-  });
   sceneController.addScene(coversScene);
 
   if (i == slides.length - 1) break;
@@ -45,21 +58,21 @@ for (let i = 0; i < slides.length; i++) {
   const fadeTimeline = gsap.timeline();
   fadeTimeline.fromTo(
     slides[i],
-    { scale: 1, opacity: 1 },
-    { scale: 0, opacity: 0, duration: 1 }
+    { scale: 1, opacity: 1, y: "0%" },
+    { scale: 0, opacity: 0, duration: 1, y: "-100%" }
   );
 
   const fadeScene = new ScrollMagic.Scene({
     triggerElement: slides[i + 1],
-    triggerHook: "onEnter",
+    triggerHook: 0.8,
   });
   fadeScene.setTween(fadeTimeline);
-  fadeScene.addIndicators({
-    colorStart: "white",
-    colorEnd: "white",
-    colorTrigger: "white",
-    name: "fade",
-    indent: 200,
-  });
+  // fadeScene.addIndicators({
+  //   colorStart: "white",
+  //   colorEnd: "white",
+  //   colorTrigger: "white",
+  //   name: "fade",
+  //   indent: 200,
+  // });
   sceneController.addScene(fadeScene);
 }
